4f62976e79f634cb961024d08b97bc565a86722e
Willingo1 <rollacoin6@gmail.com>
2025-03-21 16:08:55 -0700

    add comments and ensure edited and calculated dataframe is published

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,10 @@ st.write(
 
 import streamlit as st
 import pandas as pd
-
+# Define skill names
 skill_names = ["mining","logging","carpentry","pottery","masonry"]
+
+# Initialize dataframe
 df = pd.DataFrame(
     [
        {"Name": "Willingo", "Unique Skills":1},
@@ -23,7 +25,12 @@ df = pd.DataFrame(
 for col in skill_names:
     df[col]=False
 
+# Make rows addable and skills editable
 edited_df = st.data_editor(df, num_rows="dynamic")
+
+# Recalculate "Unique Skills"
 edited_df["Unique Skills"] = edited_df.select_dtypes(include=bool).sum(axis=1)
 
+# Display edited and recalculated dataframe
+st.dataframe(edited_df)
 print()
